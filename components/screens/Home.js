0211fc80e0e5424ef3f9e8fb1a112a09fd779573
RAJAN KUMAR <rajansharmaa46@gmail.com>
2022-05-14 23:02:01 +0530

    first react native ecommerce app

---
+++ components/screens/Home.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import {
+//   View,
+//   FlatList,
+//   StyleSheet,
+//   TextInput,
+//   Image,
+//   Dimensions,
+//   TouchableOpacity,
+// } from 'react-native';
